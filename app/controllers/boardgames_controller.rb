848bc5687f0a36bd0985d19e03986e23301f5132
Joel Seng <joel29dec@gmail.com>
2019-07-29 09:26:45 -0500

    backend server done

---
+++ app/controllers/boardgames_controller.rb
@@ -0,0 +1,11 @@
+class BoardgamesController < ApplicationController
+    def index
+        boardgames = Boardgame.all
+        render json: boardgames
+    end
+
+    def show
+        boardgame = Boardgame.find_by(id: params[:id])
+        render json: boardgame
+    end
+end
